9ef074a2d8b827a71331039432eaf5bc03287549
kickoke <tina@kickoke.com>
2021-03-20 17:18:42 +0000

    Moved to different repo

--- handlebars_reuse.js
+++
@@ -1,7 +0,0 @@
-//Generate Footer
-const footer = document.getElementbyID('footer');
-
-const footerText = `Tina Lüdtke | <a href="mailto:tina@kickoke.com">tina@kickoke.com</a> | <a href="https://www.linkedin.com/in/tina-luedtke/">LinkedIn</a> <br />
-2021`;
-
-const compiledFooter = Handlebars.compile((footer.innerHTML = footerText));
